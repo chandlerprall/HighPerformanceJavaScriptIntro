a631409e6197f9c426d66874e0db823e80dd9e52
Chandler Prall <chandler.prall@gmail.com>
2015-08-04 22:23:55 -0600

    stable physics

--- hpjsi.js
+++ hpjsi.js
@@ -1,10 +1,12 @@
 (function(){
 	var scene, renderer, camera,
 		camera_fov = 55,
-		tan_fov = Math.tan( ( ( Math.PI / 180 ) * camera_fov / 2 ) );
+		tan_fov = Math.tan( ( ( Math.PI / 180 ) * camera_fov / 2 ) ),
+		physics_objects = [],
+		world;
 
 	function initGoblin() {
-
+		world = new Goblin.World( new Goblin.SAPBroadphase(), new Goblin.NarrowPhase(), new Goblin.IterativeSolver() );
 	}
 
 	function initThree() {
@@ -18,7 +20,7 @@
 		document.body.appendChild( renderer.domElement );
 
 		camera = new THREE.PerspectiveCamera( camera_fov, 800 / 640, 1, 100 );
-		camera.position.set( 0, 5, 20 );
+		camera.position.set( 0, 15, 50 );
 		camera.lookAt( scene.position );
 
 		window.addEventListener( 'resize', onWindowResize );
@@ -55,7 +57,7 @@
 
 	function initFloor() {
 		var floor = new THREE.Mesh(
-			new THREE.PlaneBufferGeometry( 50, 50 ),
+			new THREE.PlaneGeometry( 50, 50, 0, 0 ),
 			new THREE.MeshPhongMaterial({
 				map: THREE.ImageUtils.loadTexture('textures/floor_diffuse.jpg'),
 				normalMap: THREE.ImageUtils.loadTexture('textures/floor_normal.png'),
@@ -64,7 +66,7 @@
 			})
 		);
 		floor.receiveShadow = true;
-		floor.quaternion.set( -1, 0, 0, 1 ).normalize();
+		floor.geometry.applyMatrix(new THREE.Matrix4().makeRotationFromQuaternion( new THREE.Quaternion( -1, 0, 0, 1 ).normalize() ));
 		floor.material.map.wrapS = floor.material.map.wrapT = THREE.RepeatWrapping;
 		floor.material.map.repeat.set( 10, 10 );
 		floor.material.map.anisotropy = 8;
@@ -73,7 +75,13 @@
 		floor.material.specularMap.wrapS = floor.material.specularMap.wrapT = THREE.RepeatWrapping;
 		floor.material.specularMap.repeat.set( 10, 10 );
 
+		var rigid_body = new Goblin.RigidBody(
+			new Goblin.BoxShape(25, 0.001, 25),
+			Infinity
+		);
+
 		scene.add( floor );
+		world.addRigidBody( rigid_body );
 	}
 
 	var letter_geometry = {};
@@ -83,7 +91,7 @@
 		}
 
 		var height = 0.5;
-		var size = 2;
+		var size = 4;
 		var text_geometry = new THREE.TextGeometry(
 			letter,
 			{
@@ -98,11 +106,12 @@
 				bevelEnabled: false
 			}
 		);
-
-		text_geometry.computeBoundingBox();
-		//text_geometry.applyMatrix(new THREE.Matrix4().setPosition( text_geometry.boundingBox.min ));
+		//text_geometry = new THREE.BoxGeometry( 1, 1, 1, 10, 10, 10 );
+		text_geometry.center();
 		//text_geometry.computeBoundingBox();
 		text_geometry.computeVertexNormals();
+
+		window.letter = text_geometry;
 
 		return letter_geometry[letter] = text_geometry;
 	}
@@ -111,6 +120,7 @@
 		new THREE.MeshPhongMaterial( { color: 0xff5533, shading: THREE.FlatShading } ), // front
 		new THREE.MeshPhongMaterial( { color: 0xff5533, shading: THREE.SmoothShading } ) // side
 	]);
+	//text_material = new THREE.MeshLambertMaterial({ color: 0x4444FF });
 	function initWord(word, spacing, position) {
 		var letters = [];
 
@@ -119,8 +129,17 @@
 			var letter_geometry = initLetter( word[i] );
 			var letter_mesh = new THREE.Mesh( letter_geometry, text_material );
 			letter_mesh.receiveShadow = letter_mesh.castShadow = true;
+			letter_offset += (letters.length > 0 ? letters[letters.length-1].geometry.boundingBox.max.x : 0) +
+				letter_geometry.boundingBox.max.x +
+				spacing;
 			letter_mesh.position.x = letter_offset;
-			letter_offset += letter_geometry.boundingBox.max.x - letter_geometry.boundingBox.min.x + spacing;
+
+			// Add bounding frame
+			var bounding_frame = new THREE.Mesh(
+				new THREE.BoxGeometry( letter_geometry.boundingBox.max.x - letter_geometry.boundingBox.min.x, letter_geometry.boundingBox.max.y - letter_geometry.boundingBox.min.y, letter_geometry.boundingBox.max.z - letter_geometry.boundingBox.min.z ),
+				new THREE.MeshBasicMaterial({ color: 0x888888, wireframe: true })
+			);
+			letter_mesh.add(bounding_frame);
 
 			scene.add( letter_mesh );
 			letters.push(letter_mesh);
@@ -137,7 +156,38 @@
 	}
 
 	function initWords() {
-		var letters = initWord( 'JavaScript', 0.15, new THREE.Vector3(0, 0.6, 0) );
+		var letters = initWord( 'JavaScript', 1, new THREE.Vector3(0, 2, 0) ),
+			letter_shapes = letters.map(function(letter) {
+				var physics_shape = goblinShapeFromGeometry(letter.geometry ),
+					rigid_body = new Goblin.RigidBody(physics_shape, 1);
+					//rigid_body = new Goblin.RigidBody(physics_shape, Infinity);
+
+				rigid_body.mesh = letter;
+				rigid_body.position.x = letter.position.x;
+				rigid_body.position.y = letter.position.y;
+				rigid_body.position.z = letter.position.z;
+
+				world.addRigidBody( rigid_body );
+				physics_objects.push( rigid_body );
+			});
+	}
+
+	function goblinShapeFromGeometry(geometry) {
+		/*return new Goblin.ConvexShape(geometry.vertices.map(function(vertex){
+			return new Goblin.Vector3( vertex.x, vertex.y, vertex.z );
+		}));*/
+		return new Goblin.MeshShape(
+			geometry.vertices.map(function( vertex ){
+				return new Goblin.Vector3( vertex.x, vertex.y, vertex.z )
+			}),
+			geometry.faces.reduce(
+				function( faces, face ) {
+					faces.push( face.a, face.b, face.c );
+					return faces;
+				},
+				[]
+			)
+		);
 	}
 
 	function onWindowResize() {
@@ -149,8 +199,54 @@
 
 	function render() {
 		requestAnimationFrame( render );
+
+		world.step( 1 / 60 );
+		physics_objects.forEach(function(body){
+			body.mesh.position.x = body.position.x;
+			body.mesh.position.y = body.position.y;
+			body.mesh.position.z = body.position.z;
+
+			body.mesh.rotation.x = body.rotation.x;
+			body.mesh.rotation.y = body.rotation.y;
+			body.mesh.rotation.z = body.rotation.z;
+			body.mesh.rotation.w = body.rotation.w;
+		});
+
+		displayContacts();
+
 		renderer.render( scene, camera );
 	}
+
+	var displayContacts = (function(){
+		var contacts = [],
+			sphere = new THREE.SphereGeometry( 0.4 ),
+			material = new THREE.MeshNormalMaterial(),
+			normal_material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+
+		return function() {
+			while ( contacts.length ) {
+				scene.remove( contacts.pop() );
+			}
+
+			var manifold = world.narrowphase.contact_manifolds.first;
+			while ( manifold ) {
+				for ( var i = 0; i < manifold.points.length; i++ ) {
+					var mesh = new THREE.Mesh( sphere, material );
+					mesh.position.copy( manifold.points[i].contact_point );
+					scene.add( mesh );
+					contacts.push( mesh );
+
+					var normal_geometry = new THREE.Geometry();
+					normal_geometry.vertices.push( mesh.position.clone() );
+					normal_geometry.vertices.push( new THREE.Vector3().copy( manifold.points[i].contact_normal ).multiplyScalar( 2 ).add( mesh.position ) );
+					var normal = new THREE.Line( normal_geometry, normal_material );
+					scene.add( normal );
+					contacts.push( normal );
+				}
+				manifold = manifold.next_manifold;
+			}
+		};
+	})();
 
 	initGoblin();
 	initThree();
